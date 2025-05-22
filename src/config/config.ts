71a6a7acbe5ddec909d8125f85cab21bb7e58db2
Padmanabha Das <padmanabhadas9647@gmail.com>
2025-05-22 17:04:59 +0530

    first commit

---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const {OPEN_WEATHER_MAP_API_KEY} = process.env;
---
+++ src/server.ts
@@ -0,0 +1,69 @@
+import {McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
+import {StdioServerTransport} from "@modelcontextprotocol/sdk/server/stdio.js";
+import {z} from 'zod';
+import axios from "axios";
+
+// Create an MCP server
+const server = new McpServer({
+    name: 'Weather Data Fetcher',
+    version: '1.0.0',
+});
+
+async function getWeatherByCity(city: string) {
+    // const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&units=metric`);
+    /*const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=b4d4bb028b0d666876ecfba6cabe947c&units=metric`);
+    const responseData = response.data;
+    if (responseData.status === 200) {
+        const forecast = responseData.list[0].main;
+        return {
+            temp: forecast.temp,
+            forecast,
+            error: responseData,
+        }
+    } else {
+        return {
+            temp: null,
+            forecast: 'Unable to fetch data',
+            error: responseData,
+        }
+    }*/
+
+    if (city.toLowerCase() === 'patiala') {
+        return {
+            temp: '30°C',
+            forecast: 'Chances of high rain',
+        }
+    }
+    if (city.toLowerCase() === 'delhi') {
+        return {
+            temp: '20°C',
+            forecast: 'Chances of high warm winds',
+        }
+    }
+    return {
+        temp: null,
+        forecast: 'Unable to fetch data',
+    }
+}
+
+// Add an addition tool
+server.tool('getWeatherByCity', {
+    city: z.string(),
+}, async ({city}) => {
+    return {
+        content: [
+            {
+                type: 'text',
+                text: JSON.stringify(await getWeatherByCity(city)),
+            },
+        ],
+    };
+});
+
+// Start receiving messages on stdin and sending messages on stdout
+async function init() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+}
+
+init();
